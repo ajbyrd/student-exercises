629cdd9c8d8b9cc2e93c4dea7db90121d9438f42
Adam Byrd <ajbyrd0723@gmail.com>
2020-01-17 15:13:04 -0600

    initializing student exercises repo

---
+++ cohort.py
@@ -0,0 +1,5 @@
+class Cohort():
+    def __init__(self, cohort_name):
+        self.cohort_name = cohort_name
+        self.cohort_students = list()
+        self.cohort_instructors = list()
---
+++ main.py
@@ -0,0 +1,18 @@
+from exercise import Exercise 
+from student import Student
+from cohort import Cohort
+# from instructor import Instructor
+
+# Creating Exercises
+chickenMonkey = Exercise("ChickenMonkey", "Python")
+urbanPlanner = Exercise("Urban Planner", "Python")
+kennel = Exercise("Kennel", "JavaScript")
+welcomeToNashville = Exercise("Welcome to Nashville", "HTML")
+
+# Creating Cohorts
+cohort36 = Cohort("Day Cohort 36")
+cohort37 = Cohort("Day Cohort 37")
+cohort20 = Cohort("Evening Cohort 20")
+
+# Creating Students
+
